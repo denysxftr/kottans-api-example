7120a11414e07176f2f486dac1fef018f963109e
Denis Sergienko <olol.toor@gmail.com>
2016-11-29 17:29:43 +0200

    add middleware

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module KottansApiExample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 1, JsonapiRails4Middleware::Middleware
   end
 end
