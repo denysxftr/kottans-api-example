68672b8e8d3478e81f3b2382c76278beacdf559c
Denis Sergienko <olol.toor@gmail.com>
2016-11-29 18:44:12 +0200

    setup doorkeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+private
+
+  def api_request?
+    request.format.symbol == :json && !devise_controller? && !warden_authenticated_user.present?
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :doorkeeper_authorize!, if: :api_request?
 
   # GET /posts
   # GET /posts.json
